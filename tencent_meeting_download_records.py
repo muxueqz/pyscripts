221128dc07cbbc04dcc12163a7af04aeb672d710
muxueqz <zhangmingyuan240@gmail.com>
2022-03-23 09:49:13 +0800

    update tencent meeting script

--- tencent_meeting_download_records.py
+++ tencent_meeting_download_records.py
@@ -11,29 +11,6 @@ async def is_downloading(browser: browser.Browser):
     await page.goto("chrome://downloads/")
 
     await page.bringToFront()
-    dm = await page.waitForFunction(
-        """
-        () => {
-            return document.querySelector('downloads-manager').shadowRoot;
-        }
-    """
-    )
-    # progress = await page.waitForFunction(
-    # """
-    #     (tag) => {
-    #         for (const tag_root of tag.querySelectorAll('downloads-item')) {
-    #             intag = tag_root.shadowRoot;
-    #             console.log(intag);
-    #             var pauseOrResume = intag.getElementById('pauseOrResume');
-    #             console.log(pauseOrResume);
-    #             if (pauseOrResume !== null){
-    #                 return true
-    #             }
-    #         }
-    #     }
-    # """,
-    # dm
-    # )
     progress = await page.evaluate(
         """
     () => {
@@ -56,29 +33,6 @@ async def is_downloading(browser: browser.Browser):
     }
     """
     )
-    # console.log(intag);
-    # console.log(pauseOrResume);
-    # progress = await dmPage.waitForFunction(
-    #   """
-    #   () => {
-    #   var count = 0;
-    #   var pause_count = 0;
-    #   var tag = document.querySelector('downloads-manager').shadowRoot;
-    #   for (const tag_root of tag.querySelectorAll('downloads-item')) {
-    #       intag = tag_root.shadowRoot;
-    #       var pauseOrResume = intag.getElementById('pauseOrResume');
-    #       count = count + 1
-    #       if (pauseOrResume !== null){
-    #           pause_count = pause_count + 1
-    #       }
-    #   }
-    #   console.log(pause_count, count)
-    #   if (pause_count === 0){
-    #       return true
-    #   }
-    #   }
-    #   """
-    # )
     await page.close()
     print(progress)
     return progress
@@ -98,7 +52,6 @@ async def main():
     items = await page.waitForSelector("div.tea-table__body > table > tbody > tr")
     items = await page.querySelectorAll("div.tea-table__body > table > tbody > tr")
     for i in items:
-        # record_date = await i.querySelector(".meeting_start_time")
         record_date = await page.evaluate(
             "(i) => i.innerText", await i.querySelector(".meeting_start_time")
         )
@@ -107,14 +60,6 @@ async def main():
             "(i) => i.click()",
             await i.querySelector(".oparation-list > .more-dropdown"),
         )
-        # await page.evaluate(
-        #     """
-        #     (i) => {
-        #     i.click()
-        #     }
-        #     """,
-        #     await i.querySelector(".oparation-list > .more-dropdown"),
-        # )
         download_button = await page.waitForSelector(
             ".meeting-record-more-dropdown > ul > li > a"
         )
